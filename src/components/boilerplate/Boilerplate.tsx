9afa0832609e39816f12010fcef509969692d314
Kubac <mytestmail23@seznam.cz>
2024-07-12 14:12:59 +0200

    sass and css modules support

--- src/components/boilerplate/Boilerplate.tsx
+++ src/components/boilerplate/Boilerplate.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import styles from "./Boilerplate.module.scss"
 import reactLogo from "@/assets/react.svg"
 import viteLogo from "/vite.svg"
 
@@ -10,14 +11,14 @@ export const Boilerplate: FC<Props> = ({ count, setCount }) => (
   <>
     <div>
       <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={viteLogo} className={styles.logo} alt="Vite logo" />
       </a>
       <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
+        <img src={reactLogo} className={`${styles.logo} react`} alt="React logo" />
       </a>
     </div>
     <h1>Vite + React</h1>
-    <div className="card">
+    <div className={styles.card}>
       <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       <p>
         Edit <code>src/App.tsx</code> and save to test HMR
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react"
 import { Boilerplate } from "@/components/boilerplate"
-import "./App.css"
+// import "./App.css"
+import "./App.scss"
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return <Boilerplate count={count} setCount={setCount} />
 }
-
-export default App
