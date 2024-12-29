763b7b2a65d7b21a942dcb205d0b9751b3f2d011
kubac <frodo23@protonmail.com>
2024-12-29 17:30:24 +0100

    Bump @typescript-eslint/parser from 7.18.0 to 8.18.2

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -4,6 +4,7 @@ const isValidJSON = (value: any): boolean => {
   try {
     JSON.parse(value)
     return true
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (e) {
     return false
   }
